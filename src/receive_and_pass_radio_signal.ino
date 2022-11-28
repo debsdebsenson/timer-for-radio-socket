77f31f27037c80f6cbf4a9277a02a3dfa35208fc
debsdebsenson <flitzifling@posteo.de>
2022-11-28 20:14:56 +0100

    added a .gitignore file and a ToDo.md file ignored by it + slightly modified receive_and_pass_radio_signal.io

--- src/receive_and_pass_radio_signal.ino
+++ src/receive_and_pass_radio_signal.ino
@@ -1,12 +1,12 @@
 /* receive_and_pass_radio_signal */
 
 int DL=1000;
-unsigned long startMillis;  //some global variables available anywhere in the program
+//unsigned long startMillis;  //some global variables available anywhere in the program
 unsigned long currentMillis;
 
 void setup() {
   Serial.begin(9600);
-  startMillis = millis();  //initial start time
+  //startMillis = millis();  //initial start time
 }
 
 void loop() {
